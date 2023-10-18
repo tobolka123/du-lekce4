e8dc28b7371382eb7728e1c6c51f2709c67dfb17
tobolka123 <144596501+tobolka123@users.noreply.github.com>
2023-10-18 17:10:31 +0200

    pridan prvni du z lekce 1
    pridal jsem ukol z 1 lekce - sklada se z 9 ukolu

---
+++ du-lekce1/src/Main.java
@@ -0,0 +1,81 @@
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void ukol1() {
+        System.out.println("Hello world!");
+    }
+    public static void ukol2() {
+        String uzivatel = "Karel";
+        System.out.println(uzivatel);
+    }
+
+    public static void ukol3() {
+        int pocetLekci = 10;
+        System.out.println(pocetLekci);
+    }
+
+    public static void ukol4() {
+        double cena = 10.0;
+        System.out.println(cena);
+    }
+
+    public static void ukol5() {
+        BigDecimal cena = BigDecimal.ZERO; // Nastavíme do proměnné hodnotu 0
+        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(BigDecimal.valueOf(0.1));
+               // operátor "+=" znamená: přičti ke stávající
+               // hodnotě navíc 0.1
+        }
+        System.out.println(cena);
+        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+    }
+
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.ZERO;
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = 120;
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: " + velikostKosile + ".");
+    }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Oprav chyby a spusť všechny metody!");
+        System.out.println("Komentáře odstraníš klávesovou "
+                +"zkratkou <Ctrl>+</> - použij lomítko "
+                +"na numerické klávesnici.");
+        ukol1();
+        ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
+
+        String jmeno = "Štěpán";
+        String prijmeni = "Stefan";
+        int datum_narozeni = 1998;
+        double pocet_sjednanych_smlouv = 93;
+        double prodana_mrkev_v_tunach = 54;
+        String SPZ = "5SR_6695";
+        double spotreba_v_litrech_na_100km = 23.5;
+        String IP = "192.168.48.39.";
+        double prodana_mrkev_na_smlouvu = prodana_mrkev_v_tunach / pocet_sjednanych_smlouv;
+        System.out.println("Prodaná mrkev na jednu smlouvu: " + prodana_mrkev_na_smlouvu);
+    }
+}
